18798e9f949c1f560ed679521a8f9e05c8f02789
nasimuk47 <nasim.uk47@gmail.com>
2023-11-06 00:40:16 +0600

    add booking  collection

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtgfrql.mongodb.net/clothes?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -24,6 +23,7 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db("Clothes").collection("Services");
+        const BookingCollection = client.db("Clothes").collection("Booking"); // Renamed Bokking to Booking
 
         app.get("/Services", async (req, res) => {
             const cursor = serviceCollection.find();
@@ -31,16 +31,38 @@ async function run() {
             res.send(result);
         });
 
-        // Send a ping to confirm a successful connection
+        app.get("/Services/:id", async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.post("/bookings", async (req, res) => {
+            const bookingData = req.body;
+            console.log(bookingData);
+
+            const result = await BookingCollection.insertOne(bookingData);
+            res.send(result);
+        });
+
+        app.get("/bookings", async (req, res) => {
+            const cursor = BookingCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
     } finally {
-        // Ensures that the client will close when you finish/error
+        // Ensure that the client will close when you finish/error
         // await client.close();
     }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
