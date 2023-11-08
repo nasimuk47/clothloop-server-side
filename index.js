56ae52d01a342190f9cdf8e5769e5f0c92476e1e
nasimuk47 <nasim.uk47@gmail.com>
2023-11-08 16:38:37 +0600

    handle bookingchange status

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: [
+            "http://localhost:5173",
+
+            "https://clothloop-auth.web.app",
+            " https://clothloop-auth.firebaseapp.com",
+        ],
 
         credentials: true,
     })
@@ -30,7 +35,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const serviceCollection = client.db("Clothes").collection("Services");
         const BookingCollection = client.db("Clothes").collection("Booking"); // Renamed Bokking to Booking
@@ -61,26 +66,26 @@ async function run() {
 
         // middlewere
 
-        const logger = (req, res, next) => {
-            console.log("log:info", req.method, req.url);
-            next();
-        };
-        const verifyToken = (req, res, next) => {
-            const token = req.header("Authorization");
-            console.log("Token in the middleware:", token);
+        // const logger = (req, res, next) => {
+        //     console.log("log:info", req.method, req.url);
+        //     next();
+        // };
+        // const verifyToken = (req, res, next) => {
+        //     const token = req.header("Authorization");
+        //     console.log("Token in the middleware:", token);
 
-            if (!token) {
-                return res.status(401).json({ message: "Unauthorized" });
-            }
+        //     if (!token) {
+        //         return res.status(401).json({ message: "Unauthorized" });
+        //     }
 
-            try {
-                const decoded = jwt.verify(token, process.env.SECRET);
-                req.user = decoded;
-                next();
-            } catch (error) {
-                return res.status(401).json({ message: "Token is not valid" });
-            }
-        };
+        //     try {
+        //         const decoded = jwt.verify(token, process.env.SECRET);
+        //         req.user = decoded;
+        //         next();
+        //     } catch (error) {
+        //         return res.status(401).json({ message: "Token is not valid" });
+        //     }
+        // };
 
         // services related api
         app.get("/Services", async (req, res) => {
@@ -201,9 +206,44 @@ async function run() {
             }
         });
 
+        // ...
+
+        // Update a booking status by ID
+        app.put("/bookings/:id/status", async (req, res) => {
+            const id = req.params.id;
+            const { status } = req.body;
+
+            if (!ObjectId.isValid(id)) {
+                return res.status(400).json({ message: "Invalid booking ID" });
+            }
+
+            try {
+                const query = { _id: new ObjectId(id) };
+                const updatedData = { $set: { status: status } };
+
+                const result = await BookingCollection.updateOne(
+                    query,
+                    updatedData
+                );
+
+                if (result.modifiedCount > 0) {
+                    res.json({
+                        message: "Booking status updated successfully",
+                    });
+                } else {
+                    res.status(404).json({ message: "Booking not found" });
+                }
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: "Internal Server Error" });
+            }
+        });
+
+        // ...
+
         // Update service done -----------------------------------------------------------
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
